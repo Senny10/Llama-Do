10ee3ccd8ed61384c6026618c377caa1e0a7d15a
Seneca Saunders <seneca.saunders@madetech.com>
2022-11-08 19:47:32 +0000

    edit server code.

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 require "sinatra"
+require "json"
+require "dotenv"
 
+
+set :static, true
 set :public_folder, File.dirname(__FILE__) + '/static'
 
-# get '/' do 
-#     "Welcome to my World"
-# end
+get '/' do 
+    content_type 'text/html'
+    send_file File.join(settings.public_folder, 'index.html')
+end
